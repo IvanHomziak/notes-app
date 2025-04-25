5e663800be56b1564d303ea56b65543d8fd90efd
ihomziak <ivan.homziak@onedatascan.com>
2025-04-25 10:13:17 +0200

    added CORS exclusion for local development

--- src/main/java/com/ihomziak/notes/config/SecurityConfig.java
+++ src/main/java/com/ihomziak/notes/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -47,6 +46,9 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.cors(withDefaults());// CORS configuration
+
+
 		http.csrf(csrf ->
 			csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 				.ignoringRequestMatchers("/api/auth/public/**")
